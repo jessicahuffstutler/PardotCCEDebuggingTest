29d4c261c4ab1e79a1a659b0afcef70eb52a271d
jessicahuffstutler <jessica.huffstutler@gmail.com>
2016-04-13 23:16:18 -0400

    the tweets have been tweeted

--- tweetbot.py
+++ tweetbot.py
@@ -1,30 +1,33 @@
 from twython import Twython, TwythonError
 import time
-#Api info goes here
+# Api info goes here
 APP_KEY = 'XXXXXXXX'
 APP_SECRET = 'XXXXXXXX'
 OAUTH_TOKEN = 'XXXXXXXX'
 OAUTH_TOKEN_SECRET = 'XXXXXXXX'
-#Use Twython to set up api access to twitter
+# Use Twython to set up api access to twitter
 api = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 try:
-	#Read the things we need to tweet
-	with open('tweets.txt', 'r+') as tweetsfile:
-		tweets = tweetsfile.readlines()
-	#Loop through all the tweets
-	for line in tweets[:]:
-		#What are we tweeting
-		print ("Trying to tweet " + line)
-		#Attemp to send the tweet
-		api.update_status(status=line)
-		#Rate limit 
-		time.sleep(5)
-	#Yey it worked
-	print ("I have tweeted AllTheThings!") 
+    # Read the things we need to tweet
+    with open('tweets.txt', 'r+') as tweetsfile:
+        tweets = tweetsfile.readlines()
+    L = []
+    # Loop through all the tweets
+    for line in tweets[:]:
+        # Attempt to send the tweet
+        if line != "\n" and line not in L and len(line) <= 140:
+            # What are we tweeting
+            print ("Trying to tweet " + line)
+            api.update_status(status=line)
+            L.append(line)
+            # Rate limit
+            time.sleep(5)
+    # Yey it worked
+    print ("I have tweeted AllTheThings!")
 
 
-except TwythonError:
-	#in case it didn't work
-	print("Something Broke :(")  
-	
+except TwythonError, e:
+    print str(e)
+    # in case it didn't work
+    # print("Something Broke :(")
